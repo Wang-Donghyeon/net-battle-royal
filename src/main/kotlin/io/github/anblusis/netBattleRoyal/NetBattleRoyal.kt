7c7beba61c1d1821c82e3178e8b07e771be944b4
anblus <dodong0800@naver.com>
2024-08-14 01:55:34 +0900

    first commit

---
+++ src/main/kotlin/io/github/anblusis/netBattleRoyal/NetBattleRoyal.kt
@@ -0,0 +1,14 @@
+package io.github.anblusis.netBattleRoyal
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class NetBattleRoyal : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
